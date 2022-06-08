a42af167e27df822579da5e20449ea3513214f4b
Yaroslav Minakov <iam.yaroslav@gmail.com>
2022-06-08 12:33:59 +0900

    pushing to github

---
+++ index.js
@@ -0,0 +1,5 @@
+import { AddRules, DeleteRules, GetRules, GetStream, GetTweetMatchingRuleIDs } from "./lib/api.js"
+
+export {
+    AddRules, DeleteRules, GetRules, GetStream, GetTweetMatchingRuleIDs
+}
